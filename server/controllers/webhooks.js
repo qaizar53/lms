1741d92d0d9e4b8cd9c8a447092da0cd83c8326a
qaizar53 <qaizarbadri110@gmail.com>
2025-10-14 00:30:57 +0530

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -19,7 +19,7 @@ export const clerkWebhooks = async (req, res) => {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_addresses[0].email_addresses,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
