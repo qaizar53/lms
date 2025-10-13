7743959853f15f4f10265270b11e16cf8f9c1e32
qaizar53 <qaizarbadri110@gmail.com>
2025-10-13 23:25:12 +0530

    first commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,15 +1,13 @@
 import { Webhook } from "svix";
-import User from "/User.js";
-import { message } from "prompt";
-// import User from "../models/user";
+import User from "../models/user";
 
 // api contoller function to manage clerk user with database
 
-export const clerkWebhooks = async (requestAnimationFrame, res) => {
+export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(JSON.stringify(requestAnimationFrame.body), {
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
