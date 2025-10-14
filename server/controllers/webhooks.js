a1c331c44cc2be8f635fc4fd95d4b53a585cea31
qaizar53 <qaizarbadri110@gmail.com>
2025-10-15 02:52:26 +0530

    STRIPE WEBHOOK SECRET

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -65,7 +65,7 @@ export const stripeWebhooks = async (request, response) => {
     let event;
 
     try {
-        event = Stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+        event = Stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
     }
     catch (err) {
         response.status(400).send(`Webhook Error: ${err.message}`);
